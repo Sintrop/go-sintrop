ad1192fe50b9ed67fbe7f11b8187edb2ae20ae82
rav-andre <aoravagnani@gmail.com>
2025-02-24 19:42:57 -0300

    update sequoia bootnode

--- params/bootnodes_sequoia.go
+++ params/bootnodes_sequoia.go
@@ -5,8 +5,7 @@ package params
 // They will be the first point of contact for nodes coming online
 // to find peers.
 var SequoiaBootnodes = []string{
-	"enode://54533f5cf71e470f52e25841fe14af966f02b4da75846057791a83c2bf51791fd78e6a3ce77669faef595826f25219d180fd1a7cb124f9ed63d10104d8a8575d@bootsequoia.edevapps.com.br:30303",
-	"enode://f9088ef47ebeb29150e9f8ba051b0e5e8428e32a65f6eafa9a4a590334139e3d7be75528bb2d943d278d8256fd8e0d1923299b655a65568fd271ab7bb55d49ae@187.89.87.254:30303",
+	"enode://135236fd6f19d8a66a44743698ca20b5dd389a80edc45d1c12f072af3bfa75a1499821e30250fc6cc1c3e99e5d731dbda1b11973418352e0ba312f3eeccf0bba@209.38.96.144:30303",
 }
 
 // Once Sequoia network has DNS discovery set up,
